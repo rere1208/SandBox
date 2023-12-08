f97cf2dbb44f737c7ca2f174dc795fcc26dc3bb9
unknown <remidescamps7@gmail.com>
2023-12-08 16:09:20 +0100

    add .musique

--- js/app.js
+++ js/app.js
@@ -6,9 +6,24 @@ var year3 = 2025; // ne plus utiliser
 const dob = 2000 
 const isAdmin = false//boolean
 const apprenant = ["Remi","tintin", "Florian"];// TABLEAU 
-
 const sac = {livre:'Bd titnin', trousse : 'stylo', skilles:["baggarre","games"]};//objet
+const disque = document.querySelector(".disque");
+const BtnPause = document.getElementById("BtnPause");
+const paragraphe = document.querySelector(".div1 p");
+setTimeout(()=>{
+    paragraphe.style.backgroundColor = "blue";
+}, 1000)
 
+BtnPause.addEventListener("click", () => {
+    disque.classList.toggle("pause");
+    if(disque.classList.contains("pause")){
+        BtnPause.textContant ="Play";
+    }else{
+        BtnPause.textContent = "Pause";
+    }
+});
+
+console.log(disque);
 console.log(apprenant[0]);
 console.log(sac.skilles[0]);
 
@@ -39,3 +54,4 @@ const CalculeAge2 = () => {
     age > 18 ? console.log("Rentre fumier") : console.log("Tu rentre pas fumier") // condition terniere meme chose que le if
 }
 CalculeAge2();
+
